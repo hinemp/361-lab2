bb41cfb953c8f1a8b63e58c19a4bb0d6c1ea2b46
hinemp <hinemp@dukes.jmu.edu>
2022-01-30 21:02:40 -0500

    asdf

--- main.c
+++ main.c
@@ -50,7 +50,6 @@ main (int argc, char **argv)
 
   // TODO: Replace this with a loop for all events.
   int arg_counter = 0;
-  printf("%s <-- argv[1]", argv[1]);
   while (arg_counter < argc) {
     // Parse to int
     event_t curr = atoi(argv[arg_counter]);
